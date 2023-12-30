dc57c6f53dbb0ad4fc5ccc52dec00ef7427ab237
aa.farokhi <info.bamakkala@gmail.com>
2023-12-30 07:58:51 +0330

    make code prettier

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@
 
 using namespace std;
 
-
 void reset_terminal();
 int start_menu();
 int getch(void);
 
-
-int main(){
+int main()
+{
     start_menu();
 }
 
-int start_menu(){
+int start_menu()
+{
     reset_terminal();
     int selected_option = 2;
     const string options[12] = {
@@ -30,28 +30,30 @@ int start_menu(){
         "   3.2 Import a Custom Map",
         "4. History",
         "5. Leaderboard",
-        "6. Exit"
-    };
+        "6. Exit"};
     while (true)
     {
-        for (int i=0; i<12; i++){
-            if (selected_option == i+1){
+        for (int i = 0; i < 12; i++)
+        {
+            if (selected_option == i + 1)
+            {
                 cout << color::rize(options[i], "Red", "Blue") << endl;
             }
-            else {
+            else
+            {
                 cout << options[i] << endl;
             }
         }
         switch (getch())
         {
-        case 65: 
+        case 65:
             selected_option--; // key up
             if (selected_option == 4)
                 selected_option = 3;
             else if (selected_option == 7)
                 selected_option = 6;
             break;
-        case 66: 
+        case 66:
             selected_option++; // key down
             if (selected_option == 4)
                 selected_option = 5;
@@ -67,33 +69,33 @@ int start_menu(){
             selected_option = 12;
         else if (selected_option <= 1)
             selected_option = 2;
-        reset_terminal();        
+        reset_terminal();
     }
 }
-
 
 /* reads from keypress, doesn't echo */
 int getch(void)
 {
     struct termios oldattr, newattr;
     int ch;
-    tcgetattr( STDIN_FILENO, &oldattr );
+    tcgetattr(STDIN_FILENO, &oldattr);
     newattr = oldattr;
-    newattr.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
     ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
     return ch;
 }
 
-void reset_terminal(){
-    #if defined _WIN32
-        system("cls");
-        //clrscr(); // including header file : conio.h
-    #elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
-        system("clear");
-        //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
-    #elif defined (__APPLE__)
-        system("clear");
-    #endif
+void reset_terminal()
+{
+#if defined _WIN32
+    system("cls");
+    // clrscr(); // including header file : conio.h
+#elif defined(__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    // std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences
+#elif defined(__APPLE__)
+    system("clear");
+#endif
 }
